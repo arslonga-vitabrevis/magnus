e3dd91148ee5cce763fc64c5494a56f28ea76525
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-10-20 17:29:46 +0900

    font-awesomeの読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
