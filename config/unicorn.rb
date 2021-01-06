daadabab2934dd2eb902e5f65fce3ce049568730
arslonga-vitabrevis <croquis.marble@gmail.com>
2021-01-06 20:56:36 +0900

    Unicornを自動デプロイ用に設定変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,18 +1,18 @@
 
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に格納
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 #アプリケーションサーバーの性能を決める(レスポンスを生成するworkerの数)
 worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory app_path
 #Unicornの起動に必要なファイルの設置場所
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 #ポート番号の指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 #エラーログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 #Railsアプリの応答を待つ上限時間
 timeout 60
 
