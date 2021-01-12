6e9e6aa368a9bfcd5c05f1645acbe714ab909936
arslonga-vitabrevis <croquis.marble@gmail.com>
2021-01-12 10:18:30 +0900

    プリコンパイル対象のパスの微修正2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,8 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app','current','javascript','packs')
-Rails.application.config.assets.paths << Rails.root.join('app','current','src')
+Rails.application.config.assets.paths << Rails.root.join('app','javascript','packs')
+Rails.application.config.assets.paths << Rails.root.join('app','javascript','src')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
