9b07621affb140475b124586f918569189214ae8
arslonga-vitabrevis <croquis.marble@gmail.com>
2021-01-18 17:06:06 +0900

    インストールしたファイルをJavaScriptでrequireする

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery");
+require("bootstrap");
 
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
