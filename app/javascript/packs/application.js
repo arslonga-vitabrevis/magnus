d9c0c816a9d56b7359f5a09162338d174acb1337
arslonga-vitabrevis <croquis.marble@gmail.com>
2021-01-21 18:25:21 +0900

    asset関連の設定を元に戻す

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-import '../src/application.scss'
+import 'jquery/dist/jquery.js'
+import 'popper.js/dist/popper.js'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
