d1b42321cfb9bf5e2fe1cd42c8fcf14230de2eb4
arslonga-vitabrevis <croquis.marble@gmail.com>
2021-01-21 18:27:50 +0900

    WebpackでjQueryとBootstrapを使う設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
-
+const Webpack = require('webpack')
+environment.plugins.prepend(
+  'provide',
+  new Webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: 'popper.js/dist/popper'
+  })
+)
 module.exports = environment
